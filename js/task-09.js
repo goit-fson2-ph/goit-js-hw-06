aa8aae4e552573430bb555fa0e00d3da8ccd2d71
Cyfred Odarve <cyfredodarve@gmail.com>
2024-02-25 20:24:13 +0800

    Update On Task Number 9 Not Changing Background

--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const changeColorButton = document.querySelector('.change-color');
-    const colorSpan = document.querySelector('.color');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
-    changeColorButton.addEventListener('click', () => {
-    const randomColor = getRandomHexColor();
+const bodyElement = document.querySelector("body");
+const spanElement = document.querySelector(".color");
+const buttonElement = document.querySelector(".change-color");
 
-    document.body.style.backgroundColor = randomColor;
+const handleClick = () => {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  spanElement.textContent = randomColor;
+};
 
-    colorSpan.textContent = randomColor;
-    });
-
-    function getRandomHexColor() {
-      return `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, '0')}`;
-    }
-});
+buttonElement.addEventListener("click", handleClick);
